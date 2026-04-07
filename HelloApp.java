8101836e5e3bf8027a3cf787057f0951acd81b4c
snehitha-dot <yugandharsnehitha@gmail.com>
2026-04-07 22:09:02 +0530

    UC5: enhanced for loop implementation

--- HelloApp.java
+++ HelloApp.java
@@ -3,12 +3,12 @@ public class HelloApp {
 
         // If no arguments
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         } 
-        // If multiple arguments
+        // Enhanced for loop
         else {
-            for (int i = 0; i < args.length; i++) {
-                System.out.println("Hello " + args[i]);
+            for (String name : args) {
+                System.out.println("Hello " + name);
             }
         }
     }
